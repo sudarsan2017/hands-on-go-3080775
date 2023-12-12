ec6d0e2afdff6c67841e85be8cbf6dba9ecffd05
SUDARSAN IMMADI <31164697+sudarsan2017@users.noreply.github.com>
2023-12-12 06:06:23 +0000

    commit

--- challenges/types-primitive/begin/main.go
+++ challenges/types-primitive/begin/main.go
@@ -1,18 +1,38 @@
 // challenges/types-primitive/begin/main.go
 package main
 
+import "fmt"
+
 // use type inference to create a package-level variable "val" and assign it a value of "global"
+var val string = "global"
 
 func main() {
 	// create a local variable "val" and assign it an int value
+	val := 42
 
 	// print the value of the local variable "val"
+	fmt.Printf("local val = %T %v\n", val, val)
 
 	// print the value of the package-level variable "val"
+	printGlobalVars()
 
 	// update the package-level variable "val" and print it again
+	updateGlobalvars()
 
 	// print the pointer address of the local variable "val"
+	//val_point := &val
+	fmt.Printf("%T local &val %v\t\n", &val, &val)
 
 	// assign a value directly to the pointer address of the local variable "val" and print the value of the local variable "val"
+	*(&val) = 100
+	fmt.Printf("local val %v\n", val)
+}
+
+func printGlobalVars() {
+	fmt.Printf("local val = %v\n", val)
+}
+
+func updateGlobalvars() {
+	val = "updated global"
+	fmt.Printf("global val =  %v\n", val)
 }
--- types/constants/begin/main.go
+++ types/constants/begin/main.go
@@ -1,15 +1,35 @@
 // types/constants/begin/main.go
 package main
 
+import (
+	"fmt"
+)
+
 // declare a constant representing pi
+const pi = 3.14159
 
 // declare a rune constant for the letter 'a'
+const a rune = 'a'
+const b int = 10
 
 func main() {
+	const b int = 20
+	fmt.Printf("b local %T %v\n", b, b)
+
+	// fmt.Printf("a: %c - %T\n", a, a)
+
+	// const a = 20
+	// fmt.Printf("a const  %T - %v\n", a, a)
+
 	// print the value and types of pi and a
 	// fmt.Printf("pi: %v - %T\n", pi, pi)
 	// fmt.Printf("a: %c - %T\n", a, a)
 
 	// use unicode package to confirm that the rune is a letter
-	// unicode.IsLetter(a)
+	// fmt.Println(unicode.IsLetter(a))
+
+	printGlobalVars()
+}
+func printGlobalVars() {
+	fmt.Printf("b global %T %v\n", b, b)
 }
